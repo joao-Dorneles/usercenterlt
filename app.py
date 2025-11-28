3fa4766669e7eea37ceadc6a045d6387528adb0f
joao-Dorneles <joaopedrodvv@gmail.com>
2025-11-28 17:36:39 -0300

    add gitignore

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'X9f@3Qp!vT7#Lm2^dS5%hR8&kW1*Zc4$Nq6@yB0!uF3^Jt'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ltusers_user:CcXX7JTFb5SqLY6X0GLK10zKb1rw1YRw@dpg-d4kvp13uibrs73foq340-a.oregon-postgres.render.com/ltusers'
 
+db = SQLAlchemy(app)
 @app.route("/")
 def index():
     return render_template("index.html")
