f8445e0cc7f77c61f453077a96f3f6a771eeeb32
joao-Dorneles <joaopedrodvv@gmail.com>
2025-11-29 17:52:11 -0300

    conexao simples com bd

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
 
 load_dotenv()
 
@@ -19,12 +20,39 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-@app.route("/")
+class usuarios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    email = db.Column(db.String(150))
+    cpf = db.Column(db.String(14))
+    senha_hash = db.Column(db.String(255), nullable=False)
+
+    def set_senha(self, senha):
+        self.senha_hash = generate_password_hash(senha) 
+
+    def check_senha(self, senha):
+        return check_password_hash(self.senha_hash, senha)
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        return "Boas vindas"
     return render_template("index.html")
 
-@app.route("/cadastro")
+@app.route("/cadastro", methods=['GET', 'POST'])
 def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        cpf = request.form['cpf']
+
+        novo_usuario = usuarios(nome=nome, email=email, cpf=cpf)
+        novo_usuario.set_senha(senha)
+        db.session.add(novo_usuario)
+        db.session.commit()
+        return "Esta cadastrado"    
     return render_template("cadastro.html")
 
 @app.route("/hubjogos")
