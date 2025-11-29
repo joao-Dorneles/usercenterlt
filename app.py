b62ae9b3408a5c8a9b307fc57ad995c58ef30510
joao-Dorneles <joaopedrodvv@gmail.com>
2025-11-29 11:16:13 -0300

    psycopg compativel com o python do render... eu espero

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev-secret-if-missing")
 database_url = os.getenv("DATABASE_URL")
 if database_url:
     if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
+        database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
