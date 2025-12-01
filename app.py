62230c8bee22ccdacd5d13dd5c91b5d07bc5a4c0
joao-Dorneles <joaopedrodvv@gmail.com>
2025-12-01 16:32:15 -0300

    adicao do pool rcycle

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ if database_url:
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "pool_recycle": 1800 
+}
 
 db = SQLAlchemy(app)
 #aha
